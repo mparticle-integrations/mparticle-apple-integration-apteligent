64ce801e7afbd03d4945e31efe4003b485231140
Peter Jenkins <pjenkins@mparticle.com>
2019-05-22 14:29:12 -0400

    Auto-indent source code

--- mParticle-Apteligent/MPKitApteligent.m
+++ mParticle-Apteligent/MPKitApteligent.m
@@ -25,116 +25,116 @@
 #pragma mark MPKitInstanceProtocol methods
 - (MPKitExecStatus *)didFinishLaunchingWithConfiguration:(NSDictionary *)configuration {
     MPKitExecStatus *execStatus = nil;
-
+    
     NSString *appId = configuration[@"appid"];
     if (!appId) {
         execStatus = [[MPKitExecStatus alloc] initWithSDKCode:[[self class] kitCode] returnCode:MPKitReturnCodeRequirementsNotMet];
         return execStatus;
     }
-
+    
     [Crittercism enableWithAppID:appId];
-
+    
     _configuration = configuration;
     _started = YES;
-
+    
     dispatch_async(dispatch_get_main_queue(), ^{
         NSDictionary *userInfo = @{mParticleKitInstanceKey:[[self class] kitCode]};
-
+        
         [[NSNotificationCenter defaultCenter] postNotificationName:mParticleKitDidBecomeActiveNotification
                                                             object:nil
                                                           userInfo:userInfo];
     });
-
+    
     execStatus = [[MPKitExecStatus alloc] initWithSDKCode:[[self class] kitCode] returnCode:MPKitReturnCodeSuccess];
     return execStatus;
 }
 
 - (MPKitExecStatus *)leaveBreadcrumb:(MPEvent *)event {
     [Crittercism leaveBreadcrumb:event.name];
-
+    
     MPKitExecStatus *execStatus = [[MPKitExecStatus alloc] initWithSDKCode:@(MPKitInstanceCrittercism) returnCode:MPKitReturnCodeSuccess];
     return execStatus;
 }
 
 - (MPKitExecStatus *)logCommerceEvent:(MPCommerceEvent *)commerceEvent {
     MPKitExecStatus *execStatus = [[MPKitExecStatus alloc] initWithSDKCode:@(MPKitInstanceCrittercism) returnCode:MPKitReturnCodeSuccess forwardCount:0];
-
+    
     if (commerceEvent.action == MPCommerceEventActionPurchase || commerceEvent.action == MPCommerceEventActionRefund) {
         NSString *name = [commerceEvent actionNameForAction:commerceEvent.action];
         int revenueInCents = (int)floor([commerceEvent.transactionAttributes.revenue doubleValue] * 100);
         [Crittercism beginUserflow:name withValue:revenueInCents];
-
+        
         if (commerceEvent.action == MPCommerceEventActionPurchase) {
             [Crittercism endUserflow:name];
         } else {
             [Crittercism failUserflow:name];
         }
-
+        
         [execStatus incrementForwardCount];
     } else {
         NSArray *expandedInstructions = [commerceEvent expandedInstructions];
-
+        
         for (MPCommerceEventInstruction *commerceEventInstruction in expandedInstructions) {
             [self logEvent:commerceEventInstruction.event];
             [execStatus incrementForwardCount];
         }
     }
-
+    
     return execStatus;
 }
 
 - (MPKitExecStatus *)logEvent:(MPEvent *)event {
     [Crittercism leaveBreadcrumb:event.name];
-
+    
     MPKitExecStatus *execStatus = [[MPKitExecStatus alloc] initWithSDKCode:@(MPKitInstanceCrittercism) returnCode:MPKitReturnCodeSuccess];
     return execStatus;
 }
 
 - (MPKitExecStatus *)logScreen:(MPEvent *)event {
     [Crittercism leaveBreadcrumb:event.name];
-
+    
     MPKitExecStatus *execStatus = [[MPKitExecStatus alloc] initWithSDKCode:@(MPKitInstanceCrittercism) returnCode:MPKitReturnCodeSuccess];
     return execStatus;
 }
 
 - (MPKitExecStatus *)logException:(NSException *)exception {
     [Crittercism logHandledException:exception];
-
+    
     MPKitExecStatus *execStatus = [[MPKitExecStatus alloc] initWithSDKCode:@(MPKitInstanceCrittercism) returnCode:MPKitReturnCodeSuccess];
     return execStatus;
 }
 
 - (MPKitExecStatus *)setLocation:(CLLocation *)location {
     [Crittercism updateLocation:location];
-
+    
     MPKitExecStatus *execStatus = [[MPKitExecStatus alloc] initWithSDKCode:@(MPKitInstanceCrittercism) returnCode:MPKitReturnCodeSuccess];
     return execStatus;
 }
 
 - (MPKitExecStatus *)setOptOut:(BOOL)optOut {
     [Crittercism setOptOutStatus:optOut];
-
+    
     MPKitExecStatus *execStatus = [[MPKitExecStatus alloc] initWithSDKCode:@(MPKitInstanceCrittercism) returnCode:MPKitReturnCodeSuccess];
     return execStatus;
 }
 
 - (MPKitExecStatus *)setUserAttribute:(NSString *)key value:(NSString *)value {
     [Crittercism setValue:value forKey:key];
-
+    
     MPKitExecStatus *execStatus = [[MPKitExecStatus alloc] initWithSDKCode:@(MPKitInstanceCrittercism) returnCode:MPKitReturnCodeSuccess];
     return execStatus;
 }
 
 - (MPKitExecStatus *)setUserIdentity:(NSString *)identityString identityType:(MPUserIdentity)identityType {
     MPKitReturnCode returnCode;
-
+    
     if (identityType == MPUserIdentityCustomerId) {
         [Crittercism setUsername:identityString];
         returnCode = MPKitReturnCodeSuccess;
     } else {
         returnCode = MPKitReturnCodeUnavailable;
     }
-
+    
     MPKitExecStatus *execStatus = [[MPKitExecStatus alloc] initWithSDKCode:@(MPKitInstanceCrittercism) returnCode:returnCode];
     return execStatus;
 }
